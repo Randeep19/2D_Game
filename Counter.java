5088f50cd4b065ca7f2b37cdf84511ba0b723d73
Randeep19 <117176818+Randeep19@users.noreply.github.com>
2023-02-06 21:31:23 +0000

    the initial score has to be 0

--- Counter.java
+++ Counter.java
@@ -8,6 +8,9 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  */
 public class Counter extends Actor
 {
+  
+    int score = 0;
+    
     /**
      * Act - do whatever the Counter wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
