bd281328617866b125d4131b773b0181010fd6c5
Randeep19 <117176818+Randeep19@users.noreply.github.com>
2023-02-06 21:27:50 +0000

    add counter

--- MyWorld.java
+++ MyWorld.java
@@ -7,6 +7,8 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  */
 public class MyWorld extends World
 {
+ 
+    Counter counter = new Counter();
 
     /**
      * Constructor for objects of class MyWorld.
